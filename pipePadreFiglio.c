56c92fceb4bc8f12ede0cac163348390c02e1183
Luis Armando <luis.anescu@iisviolamarchesini.edu.it>
2024-02-01 09:17:07 +0100

    Modifica esercizio pipe padre e figlio

--- pipePadreFiglio.c
+++ pipePadreFiglio.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MAX  5
 int main(int argc, char *argv[])
 {
     int fd[2];
     int p;
-    int numeri[5];
     pipe(fd);
     p = fork();
     if (p != 0) // padre
     {
+
         int moltiplicatore;
-        printf("Ciao sono il padre, mio PID=%d, PID mio figlio= %d \n", getpid(), p);
+        int numeri[MAX];
         close(fd[1]);
-        read(fd[0], numeri, 20);
+        printf("Ciao sono il padre, mio PID=%d, PID mio figlio= %d \n", getpid(), p);
+        read(fd[0], numeri, sizeof(numeri));
         printf("Inserisci il numero che deve moltiplicare \n");
         scanf("%d", &moltiplicatore);
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < MAX; i++)
         {
-            printf("%d \n", numeri[i] * moltiplicatore);
+            printf("numero %d \n", numeri[i] * moltiplicatore);
         }
         close(fd[0]);
     }
     else // figlio
     {
-        printf("Ciao sono il figlio, mio PID=%d, PID mio padre= %d \n", getpid(), getppid());
+        int numeri[MAX];
         close(fd[0]);
-        for (int i = 0; i < 5; i++)
+        printf("Ciao sono il figlio, mio PID=%d, PID mio padre= %d \n", getpid(), getppid());
+        for (int i = 0; i < MAX; i++)
         {
-            printf("Inserisci il %d numero", i + 1);
+            printf("Inserisci il %d numero \n", i + 1);
             scanf("%d", &numeri[i]);
         }
-        write(fd[1], numeri, 20);
+        write(fd[1], numeri, sizeof(numeri));
         close(fd[1]);
     }
     return 0;
