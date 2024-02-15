42d537aac3838de8cac0fa032cdcbefc1a27190e
Luis Armando <luis.anescu@iisviolamarchesini.edu.it>
2024-02-15 08:30:19 +0100

    Aggiunta esercizio memoria

--- esMemoria.c
+++ esMemoria.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int global1=130;
+int global1 = 130;
 
 int main(int argc, char *argv[])
 {
     printf("lezione memoria processo linux \n");
     printf("ciao \n");
+    int a;
+    while (1)
+    {
+        printf("%d\n",getpid());
+        printf("%p\n",&a);
+        sleep(10);
+    }
+
     return 0;
 }
