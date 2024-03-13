d9d2e041c0b4bb81fe73b8ee8424bb67fed404b6
aa <aa@Usbuntus.myguest.virtualbox.org>
2024-03-13 16:09:54 +0100

    non va quella monca di una chat

--- chat1.c
+++ chat1.c
@@ -12,23 +12,40 @@ int main(int argc, char *argv[])
 {
     // Dichiarazioni Var
     int fd1;
-    int fd2;
+    int fd2, p;
     char lettura[BUFFER_SIZE];
     char scrittura[BUFFER_SIZE];
-    fd1 = open("scrittura", O_WRONLY);
-    fd2 = open("lettura", O_RDONLY);
-    printf("Scrivi HALT se vuoi finire la chat \n");
-    printf("Chatter 1: ");
-    scanf("%s", scrittura);
-    write(fd1, scrittura, sizeof(scrittura));
-    do
-    {        
-        read(fd2, lettura, sizeof(BUFFER_SIZE));
-        printf("Chatter 2: %s \n", lettura);
-        printf("Chatter 1: ");
-        scanf("%s", scrittura);
-        write(fd1, scrittura, sizeof(scrittura));
-    } while (strcmp(lettura, "HALT") != 0);
+    printf("Scrivi HALT per terminare la chat");
+    p = fork();
+    if (p != 0)
+    {
+        fd1 = open("scrittura", O_WRONLY);
+
+        while (1)
+        {
+            if (strcmp(scrittura, "HALT") == 0)
+            {
+                break;
+            }
+            printf("Chatter 1: ");
+            scanf("%s", scrittura);
+            write(fd1, scrittura, sizeof(scrittura));
+        }
+    }
+    else
+    {
+        fd2 = open("lettura", O_RDONLY);
+
+        while (1)
+        {
+            read(fd2, lettura, sizeof(BUFFER_SIZE));
+            printf("Chatter 2: %s \n", lettura);
+            if (strcmp(lettura, "HALT") == 0)
+            {
+                break;
+            }
+        }
+    }
     close(fd1);
     close(fd2);
     return 0;
--- chat2.c
+++ chat2.c
@@ -10,23 +10,42 @@
 #define BUFFER_SIZE 2048
 int main(int argc, char *argv[])
 {
-    // Dichiarazioni Var
     int fd1;
-    int fd2;
+    int fd2, p;
     char lettura[BUFFER_SIZE];
     char scrittura[BUFFER_SIZE];
-    fd1 = open("scrittura", O_RDONLY);
-    fd2 = open("lettura", O_WRONLY);
-    printf("Scrivi HALT se vuoi finire la chat \n");
-    do
+
+    printf("Scrivi HALT per terminare la chat");
+    p = fork();
+    if (p != 0)
     {
-        read(fd1, lettura, sizeof(BUFFER_SIZE));
-        printf("Chatter 1: %s \n", lettura);
-        printf("Chatter 2: ");
-        scanf("%s", scrittura);
-        write(fd2, scrittura, sizeof(scrittura));
-        
-    } while (strcmp(lettura, "HALT") != 0);
+        fd2 = open("lettura", O_WRONLY);
+
+        while (1)
+        {
+            if (strcmp(scrittura, "HALT") == 0)
+            {
+                break;
+            }
+            printf("Chatter 2: ");
+            scanf("%s", scrittura);
+            write(fd2, scrittura, sizeof(scrittura));
+        }
+    }
+    else
+    {
+        fd1 = open("scrittura", O_RDONLY);
+        while (1)
+        {
+            read(fd1, lettura, sizeof(BUFFER_SIZE));
+            printf("Chatter 1: %s \n", lettura);
+            if (strcmp(lettura, "HALT") == 0)
+            {
+                break;
+            }
+        }
+    }
+
     close(fd1);
     close(fd2);
     return 0;
