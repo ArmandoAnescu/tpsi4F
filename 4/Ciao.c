d32ce464178bf61c50c7d395857c184296ac28ca
aa <aa@Usbuntus.myguest.virtualbox.org>
2024-04-16 09:38:41 +0200

    Reorganization

--- 4/Ciao.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-int main(int argc, char *argv[])
-{
-    printf("%s \n",argv[0]);
-    printf("Il mio pid 2.0 è %d\n", getpid());
-    return 0;
-}
