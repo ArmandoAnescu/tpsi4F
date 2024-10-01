7b3f8d4a465717fbe7583e1314078b158e5c12bc
Armando <luis.anescu@iisviolamarchesini.edu.it>
2024-10-01 15:25:13 +0200

    Finito es ripasso stringhe

--- 5/ripasso2.c
+++ 5/ripasso2.c
@@ -5,9 +5,9 @@ void ControlloStringa(char stringa[])
 {
     for (int i = 0; i < strlen(stringa); i++)
     {
-        if (stringa[i] < 97 || stringa[i] > 122)
+        if (stringa[i] < 'a' || stringa[i] > 'z')
         {
-            if (stringa[i] < 65 || stringa[i] > 90)
+            if (stringa[i] < 'A' || stringa[i] > 'Z')
             {
                 printf("Numero presente\n");
                 exit(1);
@@ -88,6 +88,76 @@ void ComparaLunghezza(char str1[], char str2[])
         printf("la prima stringa è più corta della seconda \n");
     }
 }
+void ContaVocali(char str1[], char str2[])
+{
+    int nVocali1 = 0, nVocali2 = 0;
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        if (str1[i] == 'a' || str1[i] == 'e' || str1[i] == 'i' || str1[i] == 'o' || str1[i] == 'u')
+        {
+            nVocali1++;
+        }
+    }
+    for (int i = 0; i < strlen(str2); i++)
+    {
+        if (str2[i] == 'a' || str2[i] == 'e' || str2[i] == 'i' || str2[i] == 'o' || str2[i] == 'u')
+        {
+            nVocali2++;
+        }
+    }
+    if (nVocali1 > nVocali2)
+    {
+        printf("La stringa 1 ha più vocali della stringa 2 \n");
+    }else{
+        printf("La stringa 2 ha più vocali della stringa 1 \n");
+    }
+}
+void ContaConsonanti(char str1[], char str2[])
+{
+    int nVocali1 = 0, nVocali2 = 0;
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        if (str1[i] != 'a' || str1[i] != 'e' || str1[i] != 'i' || str1[i] != 'o' || str1[i] != 'u')
+        {
+            nVocali1++;
+        }
+    }
+    for (int i = 0; i < strlen(str2); i++)
+    {
+        if (str2[i] != 'a' || str2[i] != 'e' || str2[i] != 'i' || str2[i] != 'o' || str2[i] != 'u')
+        {
+            nVocali2++;
+        }
+    }
+    if (nVocali1 > nVocali2)
+    {
+        printf("La stringa 1 ha più consonanti della stringa 2 \n");
+    }else{
+        printf("La stringa 2 ha più consonanti della stringa 1 \n");
+    }
+}
+void LetterePresenti(char str1[],char str2[]){
+    char presenti[10];
+    int n=0;
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        for (int j = 0; j < strlen(str2); j++)
+        {
+            if(str1[i]==str2[j]){
+                presenti[n]=str1[i];
+                n++;
+            }
+        }
+        
+    }
+    printf("le lettere presenti sono:\n");
+    for (int i = 0; i < strlen(presenti); i++)
+    {
+        printf("%c\n",presenti[i]);
+    }
+    
+    
+}
 int main()
 {
     char stringa1[30];
@@ -105,5 +175,7 @@ int main()
     printf("Inserisci la prima stringa \n");
     scanf("%s", stringa2);
     ComparaLunghezza(stringa1, stringa2);
+    ContaVocali(stringa1, stringa2);
+    ContaConsonanti(stringa1,stringa2);
     return 0;
 }
